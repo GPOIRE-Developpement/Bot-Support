7af5c482260a19d0c459e458a559ebad282518cb
GPOIRE <gpoire.developpement@gmail.com>
2024-06-05 09:16:28 +0200

    05/06

--- src/commands/info/cooldowns.js
+++ src/commands/info/cooldowns.js
@@ -42,6 +42,6 @@ module.exports = {
     
         const endDate = dateObj.setMonth(dateObj.getMonth()+1);
 
-        interaction.reply(client.embeds.certificateChooseDomain(memberID, startDate, endDate, reason))
+        interaction.reply(client.embeds.certificateChooseDomain(memberID, dateObj.getDay()+"/"+dateObj.getMonth()+"/"+dateObj.getFullYear(), endDate, reason))
     }
 }; 
